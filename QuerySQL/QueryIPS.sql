9815189a8160c47748c314020c73f5ff0d1a224c
Syro23 <127954515+Syro23@users.noreply.github.com>
2024-04-26 18:11:26 -0500

    Añadir sedes y consultorios

--- QuerySQL/QueryIPS.sql
+++ QuerySQL/QueryIPS.sql
@@ -109,3 +109,35 @@ alter table tratamiento_medicamento add constraint FK_Tratamiento_Medicamento fo
 alter table tratamiento_medicamento add constraint FK_MedicamentoTrat foreign key (cod_medicamento) references medicamento(codigo);
 
 alter table historial_clinico add constraint FK_HistorialPac foreign key (ID_Paciente) references paciente(ID_Paciente);
+
+
+-- Valores Sedes
+insert into sede(nombre, direccion) values
+('Floridablanca', 'Av'),
+('Bucaramanga', 'Cll'),
+('Piedecuesta', 'Cra'),
+('Lebrija', 'Cra'),
+('Pamplona', 'Cll'),
+('Rionegro', 'Cra');
+
+select * from consultorio;
+-- Valores Consultorios
+insert into consultorio(nombre, CodSede) values
+('101', 1), ('102', 1), ('103', 1), ('104', 1), ('105', 1),
+('201', 1), ('202', 1), ('203', 1), ('204', 1), ('205', 1),
+('301', 1), ('302', 1), ('303', 1), ('304', 1), ('305', 1),
+('101', 2), ('102', 2), ('103', 2), ('104', 2), ('105', 2),
+('201', 2), ('202', 2), ('203', 2), ('204', 2), ('205', 2),
+('301', 2), ('302', 2), ('303', 2), ('304', 2), ('305', 2),
+('101', 3), ('102', 3), ('103', 3), ('104', 3), ('105', 3),
+('201', 3), ('202', 3), ('203', 3), ('204', 3), ('205', 3),
+('301', 3), ('302', 3), ('303', 3), ('304', 3), ('305', 3),
+('101', 4), ('102', 4), ('103', 4), ('104', 4), ('105', 4),
+('201', 4), ('202', 4), ('203', 4), ('204', 4), ('205', 4),
+('301', 4), ('302', 4), ('303', 4), ('304', 4), ('305', 4),
+('101', 5), ('102', 5), ('103', 5), ('104', 5), ('105', 5),
+('201', 5), ('202', 5), ('203', 5), ('204', 5), ('205', 5),
+('301', 5), ('302', 5), ('303', 5), ('304', 5), ('305', 5),
+('101', 6), ('102', 6), ('103', 6), ('104', 6), ('105', 6),
+('201', 6), ('202', 6), ('203', 6), ('204', 6), ('205', 6),
+('301', 6), ('302', 6), ('303', 6), ('304', 6), ('305', 6);
